60f327cee7389ade01ac16fd8b627caa54d84e41
LordFedot~ <you@example.com>
2024-03-25 18:22:48 +0200

    fix

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -8,7 +8,6 @@ interface AvatarProps {
 }
 
 const Avatar = ({ user }: AvatarProps) => {
-  const isActive = true;
   return (
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
@@ -18,24 +17,6 @@ const Avatar = ({ user }: AvatarProps) => {
           src={user?.image || "/images/placeholder.jpg"}
         />
       </div>
-      {isActive ? (
-        <span
-          className="
-            absolute 
-            block 
-            rounded-full 
-            bg-green-500 
-            ring-2 
-            ring-white 
-            top-0 
-            right-0
-            h-2 
-            w-2 
-            md:h-3 
-            md:w-3
-          "
-        />
-      ) : null}
     </div>
   );
 };
